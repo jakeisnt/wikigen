cca1c71487beb45e0085f0f8aaf1f32c7aa5071d
Jacob Chvatal <jake@isnt.online>
2021-02-17 22:40:27 -0800

    Reworked - mostly - with pandoc

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    orgmode-parse.url = "github:jakeisnt/orgmode-parse/master";
     utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,25 +12,10 @@
         inherit (lib) attrValues;
         compiler = "ghc8104";
         lib = nixpkgs.lib;
-        orgmode-parse = with pkgs.haskellPackages;
-          callPackage ./orgmode-parse.nix { };
-
-        overlay = (self: super:
-          let changeset = { orgmode-parse = orgmode-parse; };
-          in {
-            haskellPackages = super.haskellPackages.override {
-              overrides = hself: hsuper: hsuper // changeset;
-            };
-          });
-
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-        };
+        pkgs = import nixpkgs { inherit system; };
         package = with pkgs.haskellPackages; callPackage (callCabal2nix "wikigen" ./.) { };
       in {
         defaultPackage = package;
-        defaultOverlay = overlay;
         devShell = package.env;
       });
 }

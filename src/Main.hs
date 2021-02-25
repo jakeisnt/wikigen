889ba7f08dc69bc0bc5f0c3461b7652b8805c14f
Jacob Chvatal <jake@isnt.online>
2021-02-24 18:07:00 -0800

    Factor out html mod code

--- src/Main.hs
+++ src/Main.hs
@@ -15,10 +15,7 @@ import System.FilePath
 import Text.Pandoc.Builder
 import System.Directory
 import Wikigen.File.Utils (addNDirectory, addDirectory)
-
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import qualified Text.Blaze.Html.Renderer.String as H
+import Wikigen.Html (unparseHtml)
 
 -- cli options
 data CliOpts = Generate { dirPath :: FilePath }
@@ -117,26 +114,3 @@ parseOrg t = do
   return $ fromRight (error "bad") maybeFile
 
 
-newtype Title = Title String
-
--- write Html to a text buffer
-unparseHtml :: Pandoc -> IO T.Text
-unparseHtml ast = do
-  html <- runIO (writeHtml5String def ast) >>= handleError 
-  return $ augmentHtml (Title "title") html
-
-  where
-    -- add additional information to the html exported
-    augmentHtml :: Title -> Text -> Text
-    augmentHtml tt = T.pack . H.renderHtml . (augmentBlaze tt) . H.preEscapedToHtml
-    
-    -- add some information to the header that blaze html neglected to
-    augmentBlaze :: Title -> H.Html -> H.Html
-    augmentBlaze (Title t) html =
-      H.docTypeHtml $ do
-      H.head $ do
-        H.title $ H.toHtml t
-        H.link H.! A.rel "stylesheet" H.! A.type_ "text/css" H.! A.href "https://jakeisnt.github.io/styles/main.css"
-      H.body $ do
-        H.title $ H.toHtml t
-        html

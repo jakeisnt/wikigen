467af841af87a38d7e7a1539ae18f5793593062c
Jacob Chvatal <jake@isnt.online>
2021-02-24 18:05:10 -0800

    snag title from pandoc

--- src/Main.hs
+++ src/Main.hs
@@ -15,8 +15,9 @@ import System.FilePath
 import Text.Pandoc.Builder
 import System.Directory
 import Wikigen.File.Utils (addNDirectory, addDirectory)
+
 import qualified Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html.Renderer.String as H
 
 -- cli options
@@ -115,22 +116,27 @@ parseOrg t = do
   maybeFile <- runIO $ readOrg def t
   return $ fromRight (error "bad") maybeFile
 
+
+newtype Title = Title String
+
 -- write Html to a text buffer
 unparseHtml :: Pandoc -> IO T.Text
 unparseHtml ast = do
-  html <- runIO (writeHtml5String def ast) >>= handleError -- pandocToHtml ast -- >>= handleError
-  return $ augmentHtmlResult html
+  html <- runIO (writeHtml5String def ast) >>= handleError 
+  return $ augmentHtml (Title "title") html
 
   where
-    augmentHtmlResult :: Text -> Text
-    augmentHtmlResult = T.pack . H.renderHtml . augmentBlaze . H.preEscapedToHtml
+    -- add additional information to the html exported
+    augmentHtml :: Title -> Text -> Text
+    augmentHtml tt = T.pack . H.renderHtml . (augmentBlaze tt) . H.preEscapedToHtml
     
     -- add some information to the header that blaze html neglected to
-    augmentBlaze :: H.Html -> H.Html
-    augmentBlaze html =
+    augmentBlaze :: Title -> H.Html -> H.Html
+    augmentBlaze (Title t) html =
       H.docTypeHtml $ do
       H.head $ do
-        H.title "this website deserves a title!"
+        H.title $ H.toHtml t
         H.link H.! A.rel "stylesheet" H.! A.type_ "text/css" H.! A.href "https://jakeisnt.github.io/styles/main.css"
-      H.body $ html
--- initial css source: https://github.com/susam/spcss/blob/master/sp.min.css
+      H.body $ do
+        H.title $ H.toHtml t
+        html

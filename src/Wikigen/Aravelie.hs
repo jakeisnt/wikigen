702635af0abce48addae92edb0cea6c2d2feb04d
Jacob Chvatal <jake@isnt.online>
2021-02-25 12:48:29 -0800

    Implement aravelie time conversion

--- src/Wikigen/Aravelie.hs
+++ src/Wikigen/Aravelie.hs
@@ -9,11 +9,13 @@ module Wikigen.Aravelie
 
 import Universum
 import Data.Time
+import Data.Char (ord)
 
 -- The Aravelie calendar date format.
 data Arvelie = Arvelie
-  { week :: Text
-  , day :: Integer }
+  { year :: Int
+  , week :: Char
+  , day :: Int }
   deriving (Eq, Ord, Show)
 
 -- The Neralie time format.
@@ -31,15 +33,23 @@ data AlternateTime = AlternateTime
 timeToAlternate :: LocalTime -> AlternateTime
 timeToAlternate dt = AlternateTime { date = dateToArvelie $ localDay dt, time = timeToNeralie $ localTimeOfDay dt }
 
-
 -- convert a day to its corresponding arvelie date
 -- assume initialization year of 2020
 dateToArvelie :: Day -> Arvelie
-dateToArvelie d =
-  let week = "1"
-      day = 1
+dateToArvelie dt =
+  -- get days of the year
+  -- get years since beginning (2020)
+  let (y, m, d) = toGregorian dt
+      year = fromIntegral $ y - 2020
+      -- computes the number of day
+      daysSoFar = foldl' (\cum month -> (gregorianMonthLength y month) + cum) 0 [0..(m - 1)] + d
+      -- add constant to start with letter A, divide by 14 days per week
+      weekNum = floor $ (toRational daysSoFar) / 14 + 65 
+      -- for months over 26, use '+' to denote the additional days.
+      week = if weekNum > 25 then '+' else chr weekNum
+      day = rem daysSoFar 14
   in
-  Arvelie { week, day }
+  Arvelie { year, week, day }
 
 -- convert a time to its corresponding neralie time
 timeToNeralie :: TimeOfDay -> Neralie
@@ -47,6 +57,6 @@ timeToNeralie t =
   let pulses = toRational $ ((todHour t * 60) + todMin t) * 60
       res = pulses / 86.4
       beat = floor res
-      pulse = ceiling $ res * 1000
+      pulse = floor $ res * 1000
    in
    Neralie { beat, pulse }
